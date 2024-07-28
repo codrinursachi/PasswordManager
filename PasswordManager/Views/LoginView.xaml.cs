c4432855ff0d1c42168f6e91821a022900964910
codrinursachi <codrinursachi@gmail.com>
2024-07-28 18:29:58 +0300

    Add ViewModelBase

--- PasswordManager/Views/LoginView.xaml.cs
+++ PasswordManager/Views/LoginView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PasswordManager.View
+namespace PasswordManager.Views
 {
     /// <summary>
     /// Interaction logic for LoginView.xaml
