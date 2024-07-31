4e75ae8212e8dbdd5e44b482b7f4bef247498eb9
codrinursachi <codrinursachi@gmail.com>
2024-07-31 13:42:53 +0300

    Add PasswordRepository edit test

--- PasswordManagerTests/PasswordRepositoryTests.cs
+++ PasswordManagerTests/PasswordRepositoryTests.cs
@@ -19,5 +19,16 @@ namespace PasswordManagerTests
             passwordRepository.Add(password, "admin");
             Assert.NotNull(passwordRepository.GetAllPasswords("admin").FirstOrDefault(p => p.Username == "admin" && p.Password == "admin" && p.Database == "default" && p.Url == "admin.com"));
         }
+
+        [Fact]
+        public void ShouldEditPasswordWhenRequested()
+        {
+            PasswordRepository passwordRepository = new();
+            PasswordModel password = new PasswordModel { Username = "admin", Password = "admin", Database = "default", Url = "admin.com" };
+            PasswordModel passwordEdit = new PasswordModel { Username = "root", Password = "root", Database = "default", Url = "admin.com" };
+            passwordRepository.Add(password, "admin");
+            passwordRepository.Edit(password, passwordEdit,"admin");
+            Assert.NotNull(passwordRepository.GetAllPasswords("admin").FirstOrDefault(p => p.Username == "root" && p.Password == "root" && p.Database == "default" && p.Url == "admin.com"));
+        }
     }
 }
