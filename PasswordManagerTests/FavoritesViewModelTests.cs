cd86b73ab5aeebaa00e4a2b8656973182d3e8386
codrinursachi <codrinursachi@gmail.com>
2024-08-30 11:57:55 +0300

    Add FavoritesViewModel filter test

--- PasswordManagerTests/FavoritesViewModelTests.cs
+++ PasswordManagerTests/FavoritesViewModelTests.cs
@@ -33,5 +33,24 @@ namespace PasswordManagerTests
             Assert.Equal(2, allPasswordsViewModel.Passwords.Count);
             File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PasswordManager", "Databases", file));
         }
+
+        [Fact]
+        public void ShouldFilterPasswords()
+        {
+            string file = Path.GetRandomFileName();
+            PasswordRepository passwordRepository = new(file, "admin");
+            PasswordModel password = new PasswordModel { Username = "admin", Password = "admin", Url = "admin.com" };
+            PasswordModel password2 = new PasswordModel { Username = "admin2", Password = "admin2", Url = "admin2.com", Favorite = true };
+            PasswordModel password3 = new PasswordModel { Username = "admin3", Password = "admin3", Url = "admin3.com", Favorite = true };
+            passwordRepository.Add(password);
+            passwordRepository.Add(password2);
+            passwordRepository.Add(password3);
+            FavoritesViewModel allPasswordsViewModel = new();
+            allPasswordsViewModel.Database = file;
+            allPasswordsViewModel.Password = "admin";
+            allPasswordsViewModel.SearchFilter = "admin2";
+            Assert.Single(allPasswordsViewModel.Passwords);
+            File.Delete(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PasswordManager", "Databases", file));
+        }
     }
 }
