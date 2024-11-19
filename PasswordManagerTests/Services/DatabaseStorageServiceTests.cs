344bfd1370ab35b93d4694250bf552a72c666f65
codrinursachi <codrinursachi@gmail.com>
2024-11-19 07:31:42 +0200

    Add DatabaseStorageServiceTests

---
+++ PasswordManagerTests/Services/DatabaseStorageServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using PasswordManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerTests.Services
+{
+    public class DatabaseStorageServiceTests
+    {
+        [Fact]
+        public void ShouldScanAndStoreDatabaseNames()
+        {
+            DatabaseStorageService databaseStorageService = new("test1");
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PasswordManager", "Databases", "test1");
+            File.Create(Path.Combine(path, "db1.json"));
+            File.Create(Path.Combine(path, "db2.json"));
+            databaseStorageService.Refresh();
+            Assert.Equal(["db1", "db2","default"], databaseStorageService.Databases);
+        }
+    }
+}
