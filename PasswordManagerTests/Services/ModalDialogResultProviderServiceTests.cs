7f7fef83f673a423611e72359b938e5fb3a97a16
codrinursachi <codrinursachi@gmail.com>
2024-11-26 17:42:06 +0200

    Add ModalDialogResultProviderService test

---
+++ PasswordManagerTests/Services/ModalDialogResultProviderServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using PasswordManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerTests.Services
+{
+    public class ModalDialogResultProviderServiceTests
+    {
+        [Fact]
+        public void ShouldStoreResult()
+        {
+            ModalDialogResultProviderService modalDialogResultProviderService = new()
+            {
+                Result = true
+            };
+
+            Assert.True(modalDialogResultProviderService.Result);
+        }
+    }
+}
