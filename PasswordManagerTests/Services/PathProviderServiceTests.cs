b2aef73036d4bd570f1a2e35c84aa266b15a9ed2
codrinursachi <codrinursachi@gmail.com>
2024-11-26 17:46:21 +0200

    Add PathProviderService test

---
+++ PasswordManagerTests/Services/PathProviderServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using PasswordManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerTests.Services
+{
+    public class PathProviderServiceTests
+    {
+        [Fact]
+        public void ShouldReturnCorrectPath()
+        {
+            PathProviderService pathProviderService = new();
+            Assert.Equal(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PasswordManager"),pathProviderService.ProgramPath);
+        }
+    }
+}
