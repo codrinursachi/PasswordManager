626676a326fe6cbcf0d76c3481dbec15e111b1f8
codrinursachi <codrinursachi@gmail.com>
2024-11-26 17:15:00 +0200

    Add PasswordImportDTO test

---
+++ PasswordManagerTests/DTOs/PasswordImportDTOTests.cs
@@ -0,0 +1,38 @@
+﻿using PasswordManager.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerTests.DTOs
+{
+    public class PasswordImportDTOTests
+    {
+        [Fact]
+        public void ShouldStorePasswordImportDTOData()
+        {
+            PasswordToShowDTO password = new()
+            {
+                Id = 1,
+                Username = "admin",
+                Password = "admin".ToCharArray(),
+                Url = "admin.com",
+                ExpirationDate = "No expiration",
+                CategoryPath = "admin/admin",
+                Tags = "#tag1 #tag2 #tag3",
+                Favorite = true,
+                Notes = "admin notes"
+            };
+            Assert.Equal(1, password.Id);
+            Assert.Equal("admin", password.Username);
+            Assert.Equal("admin".ToCharArray(), password.Password);
+            Assert.Equal("admin.com", password.Url);
+            Assert.Equal("No expiration", password.ExpirationDate);
+            Assert.Equal("admin/admin", password.CategoryPath);
+            Assert.Equal("#tag1 #tag2 #tag3", password.Tags);
+            Assert.True(password.Favorite);
+            Assert.Equal("admin notes", password.Notes);
+        }
+    }
+}
