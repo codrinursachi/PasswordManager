7fd9f2d492e7230fcbe2bf6b60339cceb406302a
codrinursachi <codrinursachi@gmail.com>
2024-12-03 08:20:19 +0200

    Test cleanup

--- PasswordManagerTests/Models/PasswordModelTests.cs
+++ PasswordManagerTests/Models/PasswordModelTests.cs
@@ -7,16 +7,18 @@ namespace PasswordManagerTests.Models
         [Fact]
         public void ShouldStorePasswordData()
         {
-            PasswordModel password = new();
-            password.Id = 1;
-            password.Username = "admin";
-            password.Password = "admin".ToCharArray();
-            password.Url = "admin.com";
-            password.ExpirationDate = default;
-            password.CategoryPath = "admin/admin";
-            password.Tags = "tag1 tag2 tag3";
-            password.Favorite = true;
-            password.Notes = "admin notes";
+            PasswordModel password = new()
+            {
+                Id = 1,
+                Username = "admin",
+                Password = "admin".ToCharArray(),
+                Url = "admin.com",
+                ExpirationDate = default,
+                CategoryPath = "admin/admin",
+                Tags = "tag1 tag2 tag3",
+                Favorite = true,
+                Notes = "admin notes"
+            };
             Assert.Equal(1, password.Id);
             Assert.Equal("admin", password.Username);
             Assert.Equal("admin".ToCharArray(), password.Password);
